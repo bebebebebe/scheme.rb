1b632506064f11b87438b38d50a7cb0f923ce341
Be <bebirchall@gmail.com>
2013-04-29 18:25:23 -0400

    tree change for tooltip formatting

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,17 +1,29 @@
 module Tree
 
   def self.structure(root)
+    hash = structure1(root)
+    hash[:name] = "global environment"
+    hash
+  end
+
+  def self.structure1(root)
     { id: root.id,
       frame: process_frame(root.frame, root.label), 
       children: root.children.map{ |i| structure(i) }}
-      
   end
+
+  # def self.process_frame(frame, label)
+  #   frame2 = frame.dup
+  #   string = frame2.each { |k,v| frame2[k] = schemify(label[k]) if v.class == Proc }.to_s
+  #   #string.gsub('"', '').gsub(":", "").gsub("=>", ": ")
+  #   string = string.delete('":{}').gsub("=>", ": ").gsub(", ", "<br>")
+  # end
 
   def self.process_frame(frame, label)
     frame2 = frame.dup
-    string = frame2.each { |k,v| frame2[k] = schemify(label[k]) if v.class == Proc }.to_s
-    #string.gsub('"', '').gsub(":", "").gsub("=>", ": ")
-    string.delete('":').gsub("=>", ": ")
+    frame2 = frame2.each { |k,v| frame2[k] = schemify(label[k]) if v.class == Proc }
+    #frame2 = frame2.each { |k,v|  }
+    string = frame2.to_s.delete('":{}').gsub("=>", ": ").gsub(", ", "<br>")
   end
 
   def self.schemify(exp)
