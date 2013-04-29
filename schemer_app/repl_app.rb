23656a0bcef6332ba13c9e6124b9bb34e27b10bd
Be <bebirchall@gmail.com>
2013-04-28 23:14:45 -0400

    enabled sessions

--- schemer_app/repl_app.rb
+++ schemer_app/repl_app.rb
@@ -1,39 +1,45 @@
 require 'sinatra'
+require 'securerandom'
 require 'json'
+require_relative './visual_interpreter/repl'
 require_relative './visual_interpreter/environment'
 require_relative './visual_interpreter/parser'
 require_relative './visual_interpreter/tree'
-require_relative './visual_interpreter/repl'
 
-repl = Repl.new
+
+enable :sessions
+
+
+repl_db = {}
+
 
 get '/' do
-  redirect '/form'
-end
-
-get '/form' do
+  session[:id] = SecureRandom.uuid
+  repl_db[session[:id]] = Repl.new
   erb :index
 end
 
 post '/form' do
+  if repl_db[session[:id]].nil?
+    redirect '/'
+  else
+    new_input = params[:message]
+    begin
+      value = repl_db[session[:id]].evaluate(new_input)
 
-  new_input = params[:message]
-  begin
-    value = repl.evaluate(new_input)
-
-    print_status = repl.print_status(new_input)
-    output = repl.printing
-    treeData = repl.tree
- 
-    if print_status or (output[0..4] == ". . .")
-      { error: false, value: output, tree: treeData, returnValue: output}.to_json
-    else
-      { error: false, value: output, tree: treeData, returnVaule: nil}.to_json
+      print_status = repl_db[session[:id]].print_status(new_input)
+      output = repl_db[session[:id]].printing
+      treeData = repl_db[session[:id]].tree
+   
+      if print_status or (output[0..4] == ". . .")
+        { error: false, value: output, tree: treeData, returnValue: output}.to_json
+      else
+        { error: false, value: output, tree: treeData, returnVaule: nil}.to_json
+      end
+    rescue
+      { error: false, value: nil, returnValue: ". . . oops, syntax error"}.to_json
     end
-  rescue
-    { error: false, value: nil, returnValue: ". . . oops, syntax error"}.to_json
   end
-
 end
 
 
