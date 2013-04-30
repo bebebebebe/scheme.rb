860f2bd3bc627354fb18935df96bf3ef45fa385b
Be <bebirchall@gmail.com>
2013-04-30 13:47:08 -0400

    styling update, info/instructions

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,14 +2,14 @@ module Tree
 
   def self.structure(root)
     hash = structure1(root)
-    hash[:name] = "global environment"
+    hash[:name] = "Global Environment.<br>"
     hash
   end
 
   def self.structure1(root)
     { id: root.id,
       frame: process_frame(root.frame, root.label), 
-      children: root.children.map{ |i| structure(i) }}
+      children: root.children.map{ |i| structure1(i) }}
   end
 
   # def self.process_frame(frame, label)
@@ -24,6 +24,7 @@ module Tree
     frame2 = frame2.each { |k,v| frame2[k] = schemify(label[k]) if v.class == Proc }
     #frame2 = frame2.each { |k,v|  }
     string = frame2.to_s.delete('":{}').gsub("=>", ": ").gsub(", ", "<br>")
+    #string.gsub(/:\s<br>(?!<)/, ", ")
   end
 
   def self.schemify(exp)
