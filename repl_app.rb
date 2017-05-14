67b35af464394101ef52e2e8d88fc573664d1e3a
Be <bebirchall@gmail.com>
2017-05-14 13:53:43 -0400

    Use sessions pool instead of sinatra cookie sessions

--- repl_app.rb
+++ repl_app.rb
@@ -6,26 +6,25 @@ require_relative './lib/environment'
 require_relative './lib/parser'
 require_relative './lib/tree'
 
-enable :sessions
+use Rack::Session::Pool
 
 repl_db = {}
 
 get '/' do
-  session[:id] = SecureRandom.uuid
-  repl_db[session[:id]] = Repl.new
+  session[:repl] = Repl.new
   erb :index
 end
 
 post '/form' do
-  if repl_db[session[:id]].nil?
+  if session[:repl].nil?
     redirect '/'
   else
     new_input = params[:message]
     begin
-      value = repl_db[session[:id]].evaluate(new_input)
-      print_status = repl_db[session[:id]].print_status(new_input)
-      output = repl_db[session[:id]].printing
-      treeData = repl_db[session[:id]].tree   
+      value = session[:repl].evaluate(new_input)
+      print_status = session[:repl].print_status(new_input)
+      output = session[:repl].printing
+      treeData = session[:repl].tree
       if print_status or (output[0..4] == ". . .")
         { error: false, value: output, tree: treeData, returnValue: output, session_ids: repl_db.keys.inspect }.to_json
       else
